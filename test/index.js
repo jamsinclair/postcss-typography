efb03795f7332ee8d4ae77f49618982f1b7671a6
jamsinclair <jamsinclairnz@gmail.com>
2020-08-16 08:31:29 +0900

    feat: add warning when no @typography rule is within the css

--- test/index.js
+++ test/index.js
@@ -44,6 +44,14 @@ test('typography()', t => {
 	t.end();
 });
 
+test('shows warning if @typography rule not in css', async t => {
+	const result = await postcss([typography()]).process('a { color: blue; }');
+	t.equals(result.css, 'a { color: blue; }', 'should not include typography css');
+	t.equals(result.warnings().length, 1, 'should be one warning');
+	const warningRegex = /no "@typography" rule was found/i;
+	t.assert(warningRegex.test(result.warnings()[0].text), 'should contain warning text');
+});
+
 test('fixtures', async t => {
 	const tests = fixtures.filter(filepath => {
 		return filepath.indexOf('.') !== 0;
@@ -53,9 +61,10 @@ test('fixtures', async t => {
 		const input = read(join(filepath, 'input.css'), 'utf-8');
 		const config = require(join(filepath, 'config.js'));
 		const processor = postcss([typography(config), prettify()]);
-		const result = await processor.process(input).css;
+		const result = await processor.process(input);
 
-		t.deepEqual(result, output, `should work on \`${fixture}\``);
+		t.deepEqual(result.css, output, `should work on \`${fixture}\``);
+		t.equals(result.warnings().length, 0, `should be no warnings for \`${fixture}\``);
 	});
 
 	await Promise.all(tests);
